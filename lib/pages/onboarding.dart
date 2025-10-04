13ebb2837a0f3be99db5780987fafa5e782403fb
dbooker5 <bookerotieno21@gmail.com>
2025-10-04 11:03:40 +0300

    Updated dependencies and packages

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../service/widget_support.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(child: Column(children: [
+        Image.asset(
+            'images/onboard.png'
+        ),
+        Text("The Fastest\n Food Delivery", style: AppWidget.HeadLineTextFieldStyle(),)
+      ],)
+      )
+    );
+  }
+}
---
+++ lib/service/widget_support.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppWidget{
+
+  static TextStyle HeadLineTextFieldStyle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 25,
+      fontWeight: FontWeight.bold
+    );
+  }
+}
