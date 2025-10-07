31e57b551331a3243ce4d504f33d7a924cc65fd3
dbooker5 <bookerotieno21@gmail.com>
2025-10-07 19:07:48 +0300

    feat: Add category list and enhance search bar on Home page
    This commit introduces a horizontal list of food categories and improves the search bar functionality on the Home page.
    
    Key changes include:
    - Importing `category_model` and `category_data` to fetch and display food categories.
    - Initializing and displaying a horizontal `ListView` of `CategoryTile` widgets.
    - Creating a new stateful widget `CategoryTile` to represent each category item with an image and name.
    - Redesigning the search bar into a distinct input field with a separate search icon button.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:foodgo/model/category_model.dart';
 import 'package:foodgo/service/widget_support.dart';
+import '../service/category_data.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,6 +11,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = [];
+
+
+  @override
+  void initState() {
+    categories = getCategories();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,15 +60,104 @@ class _HomeState extends State<Home> {
                 )
               ]
             ),
+            SizedBox(height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10),
+                    margin: EdgeInsets.only(right: 20),
+                    decoration: BoxDecoration(
+                        color: Color(0xFFececf8),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search food item...",
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Color(0xffef2b39),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                )
+              ],
+            ),
             SizedBox(height: 20,),
             Container(
-              margin: EdgeInsets.only(right: 20),
-              decoration: BoxDecoration(color: Color(0xFFececf8)),
-              child: TextField(),
-            ),
+              height: 60,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index){
+                  return CategoryTile(
+                      image: categories[index].image!,
+                      name: categories[index].name!,
+                      categoryindex: index.toString(),
+                  );
+                }
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class CategoryTile extends StatefulWidget {
+  String name, image, categoryindex;
+  CategoryTile({required this.name, required this.image, this.categoryindex = ""});
+
+
+  @override
+  State<CategoryTile> createState() => _CategoryTileState();
+}
+
+class _CategoryTileState extends State<CategoryTile> {
+  String track = "0";
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        track =widget.categoryindex.toString();
+        setState(() {
+
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        padding: EdgeInsets.only(left: 20, right: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Color(0xffef2b39)),
+        child: Row(children: [
+          Image.asset(
+              widget.image,
+              height: 40,
+              width: 40,
+              fit: BoxFit.cover),
+          SizedBox(width: 10,),
+          Text(
+            widget.name,
+            style: AppWidget.whiteTextFieldStyle(),
+          )
+        ],),
+      ),
+    );
+  }
+}
+
