b31b6a1ced1a65c1c18b61096d5ff8a0d2bf27b3
dbooker5 <bookerotieno21@gmail.com>
2025-10-11 16:50:53 +0300

    refactor: Remove 'new' keyword from PizzaModel instantiation
    This commit refactors the `getPizza` function by removing the redundant `new` keyword when creating instances of `PizzaModel`. This change aligns with modern Dart syntax conventions.

--- lib/service/pizza_data.dart
+++ lib/service/pizza_data.dart
@@ -2,31 +2,31 @@ import 'package:foodgo/model/pizza_model.dart';
 
 List<PizzaModel> getPizza(){
 List<PizzaModel> pizza = [];
-PizzaModel pizzaModel = new PizzaModel();
+PizzaModel pizzaModel = PizzaModel();
 
 pizzaModel.name = "Cheese Pizza";
 pizzaModel.image = "images/pizza1.png";
 pizzaModel.price = "40";
 pizza.add(pizzaModel);
-pizzaModel = new PizzaModel();
+pizzaModel = PizzaModel();
 
 pizzaModel.name = "Margherita Pizza";
 pizzaModel.image = "images/pizza2.png";
 pizzaModel.price = "80";
 pizza.add(pizzaModel);
-pizzaModel = new PizzaModel();
+pizzaModel = PizzaModel();
 
 pizzaModel.name = "Margherita Pizza";
 pizzaModel.image = "images/pizza2.png";
 pizzaModel.price = "80";
 pizza.add(pizzaModel);
-pizzaModel = new PizzaModel();
+pizzaModel = PizzaModel();
 
 pizzaModel.name = "Margherita Pizza";
 pizzaModel.image = "images/pizza2.png";
 pizzaModel.price = "80";
 pizza.add(pizzaModel);
-pizzaModel = new PizzaModel();
+pizzaModel = PizzaModel();
 
 return pizza;
 }
