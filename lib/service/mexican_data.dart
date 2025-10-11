8be0767cffaf83b5f40a4c93f618c458aaa46781
dbooker5 <bookerotieno21@gmail.com>
2025-10-11 16:52:14 +0300

    feat: Add Mexican food data service
    This commit introduces a new file, `mexican_data.dart`, to provide static data for Mexican food items.
    
    Key changes:
    - A `getMexican()` function that returns a list of `MexicanModel` objects.
    - The addition of initial data for items like "Tacos", "Burritos", and "Cheese Tacos", including their respective images and prices.

---
+++ lib/service/mexican_data.dart
@@ -0,0 +1,32 @@
+import '../model/mexican_model.dart';
+
+List<MexicanModel> getMexican(){
+  List<MexicanModel> mexican = [];
+  MexicanModel mexicanModel = MexicanModel();
+
+  mexicanModel.name = "Tacos";
+  mexicanModel.image = "images/mexican1.png";
+  mexicanModel.price = "60";
+  mexican.add(mexicanModel);
+  mexicanModel = MexicanModel();
+
+  mexicanModel.name = "Burritos";
+  mexicanModel.image = "images/mexican2.png";
+  mexicanModel.price = "60";
+  mexican.add(mexicanModel);
+  mexicanModel = MexicanModel();
+
+  mexicanModel.name = "Cheese Tacos";
+  mexicanModel.image = "images/mexican3.png";
+  mexicanModel.price = "60";
+  mexican.add(mexicanModel);
+  mexicanModel = MexicanModel();
+
+  mexicanModel.name = "Tacos";
+  mexicanModel.image = "images/mexican1.png";
+  mexicanModel.price = "60";
+  mexican.add(mexicanModel);
+  mexicanModel = MexicanModel();
+
+  return mexican;
+}
