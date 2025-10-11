5963509d887c50373bc5139fdd4c1b920af05474
dbooker5 <bookerotieno21@gmail.com>
2025-10-11 16:51:24 +0300

    feat: Add chinese data service
    This commit introduces a new file, `chinese_data.dart`, to provide static data for Chinese food items.
    
    Key changes:
    - A new function `getChinese()` that returns a list of `ChineseModel` objects.
    - Initial data for "Beef Noodles" and "Chinese Gyoza", including their images and prices.

---
+++ lib/service/chinese_data.dart
@@ -0,0 +1,32 @@
+import '../model/chinese_model.dart';
+
+List<ChineseModel> getChinese(){
+  List<ChineseModel> chinese = [];
+  ChineseModel chineseModel = ChineseModel();
+
+  chineseModel.name = "Beef Noodles";
+  chineseModel.image = "images/chinese1.png";
+  chineseModel.price = "60";
+  chinese.add(chineseModel);
+  chineseModel = ChineseModel();
+
+  chineseModel.name = "Chinese Gyoza";
+  chineseModel.image = "images/chinese2.png";
+  chineseModel.price = "70";
+  chinese.add(chineseModel);
+  chineseModel = ChineseModel();
+
+  chineseModel.name = "Beef Noodles";
+  chineseModel.image = "images/chinese1.png";
+  chineseModel.price = "60";
+  chinese.add(chineseModel);
+  chineseModel = ChineseModel();
+
+  chineseModel.name = "Chinese Gyoza";
+  chineseModel.image = "images/chinese2.png";
+  chineseModel.price = "70";
+  chinese.add(chineseModel);
+  chineseModel = ChineseModel();
+
+  return chinese;
+}
