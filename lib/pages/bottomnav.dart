e82bc4367dc23c57cfce9af8906ced7fce4133d6
dbooker5 <bookerotieno21@gmail.com>
2025-10-13 16:35:04 +0300

    feat: Add bottom navigation bar
    This commit introduces the `BottomNav` widget, a new stateful widget that implements the main navigation for the app.
    
    Key changes:
    - A new file, `lib/pages/bottomnav.dart`, is created.
    - The `curved_navigation_bar` package is used to create a curved bottom navigation bar.
    - The navigation bar includes icons for Home, Order, Wallet, and Profile.
    - State management is added to switch between the corresponding page widgets (`Home`, `Order`, `Wallet`, `Profile`) when an icon is tapped.

---
+++ lib/pages/bottomnav.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:foodgo/pages/profile.dart';
+import 'package:foodgo/pages/wallet.dart';
+import 'home.dart';
+import 'order.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  late List<Widget>pages;
+
+  late Home HomePage;
+  late Order order;
+  late Wallet wallet;
+  late Profile profilePage;
+
+  int currentTabIndex=0;
+
+  @override
+  void initState() {
+    HomePage = Home();
+    order = Order();
+    wallet = Wallet();
+    profilePage = Profile();
+
+    pages = [HomePage,order,wallet,profilePage];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+          height: 70,
+          backgroundColor: Colors.white,
+          color: Colors.black,
+          animationDuration: Duration(microseconds: 500),
+          onTap: (int index) {
+            setState(() {
+              currentTabIndex = index;
+            });
+          },
+          items: [
+            Icon(Icons.home, color: Colors.white, size: 30),
+            Icon(Icons.shopping_bag, color: Colors.white, size: 30),
+            Icon(Icons.wallet, color: Colors.white, size: 30),
+            Icon(Icons.person, color: Colors.white, size: 30),
+          ]),
+      body: pages[currentTabIndex],
+    );
+  }
+}
