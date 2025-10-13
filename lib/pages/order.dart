523a8f3ea06a9965309c0f9036dcc3aa726504df
dbooker5 <bookerotieno21@gmail.com>
2025-10-13 16:36:02 +0300

    feat: Add Order page
    This commit introduces a new `Order` page.
    
    The page is a stateless widget that displays a centered message "No active orders yet." It includes an app bar with the title "Your Orders".

---
+++ lib/pages/order.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:foodgo/service/widget_support.dart';
+
+class Order extends StatelessWidget {
+  const Order({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "Your Orders",
+          style: AppWidget.boldTextFieldStyle(),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Text(
+          "No active orders yet.",
+          style: AppWidget.SimpleTextFieldStyle(),
+        ),
+      ),
+    );
+  }
+}
