997e68c25e8b7f61754a6ac3df6586aa3e711cfe
dbooker5 <bookerotieno21@gmail.com>
2025-10-13 16:38:09 +0300

    feat: Add Wallet page
    This commit introduces a new `Wallet` page to the application.
    
    The page is a stateless widget that displays a user's wallet balance. It includes:
    - An `AppBar` with the title "My Wallet".
    - A centered icon and text to show the current balance, which is initially set to "$ 0.00".

---
+++ lib/pages/wallet.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:foodgo/service/widget_support.dart';
+
+class Wallet extends StatelessWidget {
+  const Wallet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "My Wallet",
+          style: AppWidget.boldTextFieldStyle(),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.account_balance_wallet, size: 80, color: Colors.grey),
+            SizedBox(height: 20),
+            Text(
+              "Balance: \$ 0.00",
+              style: AppWidget.SimpleTextFieldStyle(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
