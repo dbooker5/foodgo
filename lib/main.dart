b23553f74abbf83e5f3fa4304fd3376da4508950
dbooker5 <bookerotieno21@gmail.com>
2025-10-15 11:33:30 +0300

    feat: Set SignUp page as the initial route
    This commit updates the `main.dart` file to set the `SignUp` page as the initial screen of the application instead of `BottomNav`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodgo/pages/bottomnav.dart';
+import 'package:foodgo/pages/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: BottomNav(),
+      home: SignUp(),
     );
   }
 }
