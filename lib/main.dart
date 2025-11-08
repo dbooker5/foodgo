3b4e6462f528879020515da4be6a01c022a8661e
dbooker5 <bookerotieno21@gmail.com>
2025-11-08 14:48:24 +0300

    feat: Integrate flutter_dotenv for environment variables
    This commit adds the `flutter_dotenv` package to manage environment variables within the application.
    
    The `main` function is updated to load the `.env` file at startup, making environment variables available throughout the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:foodgo/pages/login.dart';
 
 import 'firebase_options.dart'; // Make sure this file exists in lib/
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   // ✅ Proper Firebase initialization for all platforms
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
